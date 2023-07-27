72626dcca3b03329b9dd663395d4ea6aaa2dbff0
cuilan <419475937@qq.com>
2023-07-27 11:47:01 +0800

    auto commit and push by zhangyan on 2023-07-27 11:47:01

---
+++ README.md
@@ -0,0 +1,5 @@
+# shell_scripts
+
+一些常用的shell脚本
+
+[auto_commit.sh](./auto_commit.sh)
---
+++ auto_commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get current branch
+br=`git branch | grep "*"`
+
+echo -e "\033[0;32mAuto commit and push to branch: [${br/* /}] \033[0m"
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="auto commit and push by `whoami` on `date +'%Y-%m-%d %H:%M:%S'`"
+
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin ${br/* /}
+

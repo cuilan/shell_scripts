14aa89930f43494f104d25694506a9c276c4f62a
cuilan <419475937@qq.com>
2023-08-28 15:32:17 +0800

    auto commit and push by cuilan on 2023-08-28 15:32:17

--- ubuntu/init_ubuntu22.sh
+++ ubuntu/init_ubuntu22.sh
@@ -4,11 +4,19 @@ set -e
 
 TZ='Asia/Shanghai'
 OS_RELEASE="$(lsb_release -cs)"
+SOURCES_LIST_URL="https://raw.githubusercontent.com/cuilan/source/main/ubuntu/sources.list"
 OZ_DOWNLOAD_URL='https://github.com/robbyrussell/oh-my-zsh.git'
 OZ_SYNTAX_HIGHLIGHTING_DOWNLOAD_URL='https://github.com/zsh-users/zsh-syntax-highlighting.git'
-OZ_CONFIG_DOWNLOAD_URL=''
+OZ_CONFIG_DOWNLOAD_URL='https://raw.githubusercontent.com/cuilan/source/main/zsh/zshrc'
+VIM_CONFIG_DOWNLOAD_URL='https://raw.githubusercontent.com/cuilan/source/main/vim/vimrc'
+DOCKER_LIST_URL='https://raw.githubusercontent.com/cuilan/source/main/docker/docker.list'
+DOCKER_CONFIG_DOWNLOAD_URL=''
 
 function sysupdate() {
+    if [ ! -f /etc/apt/sources.list.bak ]; then
+        cp /etc/apt/sources.list /etc/apt/sources.list.old
+        curl -fsSL ${SOURCES_LIST_URL} | sed "s@{{OS_RELEASE}}@${OS_RELEASE}@gi" >/etc/apt/sources.list
+    fi
     apt update -y
     apt upgrade -y
     apt install -y apt-transport-https ca-certificates \
@@ -32,29 +40,36 @@ function install_ohmyzsh() {
     if [ ! -d ~/.oh-my-zsh ]; then
         git clone --depth=1 ${OZ_DOWNLOAD_URL} ~/.oh-my-zsh
         git clone ${OZ_SYNTAX_HIGHLIGHTING_DOWNLOAD_URL} ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-        #curl -fsSL ${OZ_CONFIG_DOWNLOAD_URL} > ~/.zshrc
-        cp ${ZSH_TEMPLATES:-~/.oh-my-zsh/templates}/zshrc.zsh-template ~/.zshrc
+        curl -fsSL ${OZ_CONFIG_DOWNLOAD_URL} >~/.zshrc
+        #cp ${ZSH_TEMPLATES:-~/.oh-my-zsh/templates}/zshrc.zsh-template ~/.zshrc
         chsh -s $(grep /zsh$ /etc/shells | tail -1)
     fi
 }
 
 function config_vim() {
-
+    curl -fsSL ${VIM_CONFIG_DOWNLOAD_URL} >~/.vimrc
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
 }
 
 function install_docker() {
+    curl -fsSL ${DOCKER_LIST_URL} | sed "s@{{OS_RELEASE}}@${OS_RELEASE}@gi" >/etc/apt/sources.list.d/docker.list
+    curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+    apt update -y
+    apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    newgrp docker
 
+    # curl -fsSL ${DOCKER_CONFIG_DOWNLOAD_URL} >docker.service
+    # SYSTEMD_EDITOR="mv docker.service" systemctl edit docker
+    # systemctl daemon-reload && systemctl restart docker
 }
 
-function install_dc() {
-    curl -fsSL ${DOCKER_COMPOSE_DOWNLOAD_URL} >/usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-}
-
-#sysupdate
-#setlocale
-#settimezone
-#install_ohmyzsh
-config_vim
+# sysupdate
+# setlocale
+# settimezone
+# install_ohmyzsh
+# config_vim
 install_docker
-install_dc

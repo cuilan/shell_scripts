a6b5f626cefd785dee26ed759247bde3fd655a2b
cuilan <419475937@qq.com>
2024-01-19 16:47:40 +0800

    auto commit and push by cuilan on 2024-01-19 16:47:40

--- go/gobuild.sh
+++ go/gobuild.sh
@@ -8,5 +8,5 @@ GOLANG_IMAGE_NAME=cuilan/golang:1.21-alpine
 docker run --rm --volume $1:/work ${GOLANG_IMAGE_NAME} \
     bash -c "cd /work && go mod tidy && go mod download && go build -o main ."
 
-docker run --rm --volume $(pwd):/work ${GOLANG_IMAGE_NAME} \
-    bash -c "cd /work && go mod tidy && go mod download && go build -o main ."
+# docker run --rm --volume $(pwd):/work ${GOLANG_IMAGE_NAME} \
+    # bash -c "cd /work && go mod tidy && go mod download && go build -o main ."

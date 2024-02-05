4610ae42db7efba474c8e4fdad2202d3e6510255
cuilan <419475937@qq.com>
2024-02-05 23:16:26 +0800

    auto commit and push by cuilan on 2024-02-05 23:16:26

--- git/git_clean.sh
+++ git/git_clean.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-git rev-list --objects --all | grep "$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -5 | awk '{print$1}')"
+# 列出前10个大文件
+git rev-list --objects --all | grep "$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -10 | awk '{print$1}')"
 
-git filter-branch --force --index-filter 'git rm -rf --cached --ignore-unmatch BIGFILE' --prune-empty --tag-name-filter cat -- --all
-
-git push origin master --force
+# git rm 指定大文件
+git filter-branch --force --index-filter 'git rm -rf --cached --ignore-unmatch 大文件名称' --prune-empty --tag-name-filter cat -- --all
 
 rm -rf .git/refs/original/
 
 git reflog expire --expire=now --all
 
 git gc --prune=now
+
+git gc --aggressive --prune=now
+
+git push origin master --force

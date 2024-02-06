258d34298c8de6bd02e464c1f6f79fd3ddc8f68e
cuilan <419475937@qq.com>
2024-02-06 18:05:58 +0800

    auto commit and push by cuilan on 2024-02-06 18:05:58

--- auto_commit.sh
+++ auto_commit.sh
@@ -3,6 +3,13 @@
 # Get current branch
 br=$(git branch | grep "*")
 
+echo -e "Current git project: \033[35m$(pwd)\033[0m"
+
+if [ ! -d .git ]; then
+  echo -e "\033[31mThis directory has not been initialized with git!\033[0m"
+  exit 1
+fi
+
 echo -e "\033[0;32mAuto commit and push to branch: [${br/* /}] \033[0m"
 
 # Add changes to git.

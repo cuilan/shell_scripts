b3d4920052d24c94352b11bccc232c5bdf620c7b
cuilan <419475937@qq.com>
2024-03-20 13:53:46 +0800

    auto commit and push by cuilan on 2024-03-20 13:53:46

--- docker/docker_backup.sh
+++ docker/docker_backup.sh
@@ -23,19 +23,3 @@ docker volume create confluence_template
 # 将当前目录挂载到容器内 /backup 目录下
 # 将 /backup 目录下的 tar 解压至 /tmp 目录下
 docker run --rm --volume confluence_template:/tmp -v $(pwd):/backup cuilan/alpine tar xvf /backup/confluence_template.tar -C /tmp --strip 1
-
-# ------------
-
-docker run --rm --volume gitlab_config:/tmp --volume $(pwd):/backup cuilan/alpine tar cvf /backup/gitlab_config.tar /tmp
-docker run --rm --volume gitlab_data:/tmp --volume $(pwd):/backup cuilan/alpine tar cvf /backup/gitlab_data.tar /tmp
-docker run --rm --volume gitlab_logs:/tmp --volume $(pwd):/backup cuilan/alpine tar cvf /backup/gitlab_logs.tar /tmp
-
-docker save gitlab/gitlab-ce:13.7.4-ce.0 > gitlab_gitlab-ce_13.7.4-ce.0.tar
-
-scp gitlab_config.tar root@10.121.1.75:/home/docker-compose/gitlab
-
-weattech.com
-
-docker run --rm --volume gitlab_config:/tmp -v $(pwd):/backup cuilan/alpine tar xvf /backup/gitlab_config.tar -C /tmp --strip 1
-docker run --rm --volume gitlab_data:/tmp -v $(pwd):/backup cuilan/alpine tar xvf /backup/gitlab_data.tar -C /tmp --strip 1
-docker run --rm --volume gitlab_logs:/tmp -v $(pwd):/backup cuilan/alpine tar xvf /backup/gitlab_logs.tar -C /tmp --strip 1

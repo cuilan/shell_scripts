6a0a4b95a99fd1028c1f11b1e82adced4bfc98f8
cuilan <419475937@qq.com>
2024-12-26 17:30:14 +0800

    auto commit and push by cuilan on 2024-12-26 17:30:14

--- docker/docker_clean.sh
+++ docker/docker_clean.sh
@@ -3,3 +3,4 @@
 docker ps -a | grep "Exited" | awk '{print $1 }'| xargs docker stop
 docker ps -a | grep "Exited" | awk '{print $1 }'| xargs docker rm
 docker images | grep none | grep -v REPOSITORY | awk '{print $3 }' | xargs docker rmi
+docker images | grep "reg.weattech.com" | awk '{print $3 }' | xargs docker rmi
